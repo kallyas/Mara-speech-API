82f5ac4851c966ec01d1145b8606a843eed41b62
Kally <kallyasmedia@gmail.com>
2021-04-12 14:00:58 +0000

    React Init

---
+++ mara-speech-api/src/components/Header.js
@@ -0,0 +1,45 @@
+import "../App.css";
+
+const Header = () => {
+  return (
+    <>
+      <h1 className="center" id="headline">
+        Mara Speech API
+      </h1>
+      <div id="info">
+        <p id="info_start">Click on the microphone icon to begin speaking.</p>
+        <p id="info_speak_now">Speak now.</p>
+        <p id="info_no_speech">
+          No speech was detected. You may need to adjust your
+          <a href="//support.google.com/chrome/bin/answer.py?hl=en&answer=1407892">
+            microphone settings
+          </a>
+          .
+        </p>
+        <p id="info_no_microphone" style={{ display: "none" }}>
+          No microphone was found. Ensure that a microphone is installed and
+          that
+          <a href="//support.google.com/chrome/bin/answer.py?hl=en&answer=1407892">
+            microphone settings
+          </a>{" "}
+          are configured correctly.
+        </p>
+        <p id="info_allow">
+          Click the "Allow" button above to enable your microphone.
+        </p>
+        <p id="info_denied">Permission to use the microphone was denied.</p>
+        <p id="info_blocked">
+          Permission to use the microphone is blocked. To change, go to
+          chrome://settings/contentExceptions#media-stream
+        </p>
+        <p id="info_upgrade">
+          Web Speech API is not supported by this browser. Upgrade to{" "}
+          <a href="//www.google.com/chrome">Chrome</a>
+          version 25 or later.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ mara-speech-api/src/constants/Images.js
@@ -0,0 +1,9 @@
+import micAnimate from "../images/mic-animate.gif";
+import mic from "../images/mic.gif";
+import micSlash from "../images/mic-slash.gif";
+
+exports = {
+  micAnimate,
+  mic,
+  micSlash,
+};
