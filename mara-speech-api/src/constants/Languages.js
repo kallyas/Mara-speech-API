f337801133f52eafed7aae5d39290e0ee22dde3e
Kallyas <kallyasmedia@gmail.com>
2021-04-16 15:47:59 +0300

    add languages

---
+++ mara-speech-api/src/constants/Languages.js
@@ -0,0 +1,15 @@
+exports = {
+    "English": {
+        "en-GB": "United Kingdom",
+        "en-CA": "Canada",
+        "en-IN": "India",
+        "en-ZA": "South Africa",
+        "en-Us": "United States",
+    },
+    "Afrikaans": "af-ZA",
+    "Español": {
+        "es-DO": "República Dominicana",
+        'es-UY': 'Uruguay',
+        'es-VE': 'Venezuela'
+    }
+}
